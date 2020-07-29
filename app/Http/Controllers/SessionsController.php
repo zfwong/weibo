d6a06491e43c33c4f33172ccc4c4a6ed6fbaabe3
zF <kingswong97@gmail.com>
2020-07-29 10:36:31 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return View('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credential = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credential)) {
+            session()->flash('success', '欢迎回来');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+}
