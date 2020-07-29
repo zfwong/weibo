b5688a44120ab0a3cb518a69d4e939a71df45ede
zF <kingswong97@gmail.com>
2020-07-29 07:06:09 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return View('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return View('users.show', compact('user'));
     }
 }
