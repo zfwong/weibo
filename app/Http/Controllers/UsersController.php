6e844a58e5e22db99bd24b2e87e142c3d54197c0
zF <kingswong97@gmail.com>
2020-07-29 09:49:24 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     {
         return View('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
