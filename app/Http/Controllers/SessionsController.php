ad4c7e01cfa38a74d3a309aeaafae89bdf9c4b6e
zF <kingswong97@gmail.com>
2020-07-29 12:08:03 +0000

    更改用户资料

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -32,7 +32,7 @@ class SessionsController extends Controller
     public function destroy()
     {
         Auth::logout();
-        session()->flash('success', '您已成功退出! ');
+        session()->flash('success', '您已成功退出!');
         return redirect('login');
     }
 }
